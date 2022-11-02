88ecad2bbdc74047652104bc42a75d0f1d624ab2
Lucas Cruz <lucas.apcruz@gmail.com>
2022-11-02 02:18:35 -0300

    fix: tweets pagination

--- app.js
+++ app.js
@@ -6,32 +6,32 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const users =[
-    {username: "bobesponja", avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"}
+const users = [
+    { username: "bobesponja", avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" }
 ]
 const tweets = [
-    {username: "bobesponja", tweet:"eu amo o hub"}
+    { username: "bobesponja", tweet: "eu amo o hub" }
 ]
 
 
-app.get("/tweets", (req,res) => {
+app.get("/tweets", (req, res) => {
     const payload = []
     const page = Number(req.query.page)
-    if(page<1){
+    if (page < 1) {
         res.status(400).send("Informe uma página válida!")
-    }else{
-        const end = Math.min(tweets.length, (page*10))
+    } else {
+        const end = Math.min(tweets.length, (page * 10))
         const start = end - 10
         const tweetsWindow = tweets.slice(start, end)
         console.log(tweetsWindow)
-        tweets.forEach((tweet) => {
-        const user = users.find((user) => user.username === tweet.username)
-        const avatar = user.avatar
-        tweet.avatar = avatar 
-        payload.push(tweet)
-    })
+        tweetsWindow.forEach((tweet) => {
+            const user = users.find((user) => user.username === tweet.username)
+            const avatar = user.avatar
+            tweet.avatar = avatar
+            payload.push(tweet)
+        })
 
-    res.send(payload)
+        res.send(payload)
     }
 })
 
