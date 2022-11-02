63e0e93d51e278118d9eda472eeee9ad54dba91b
Lucas Cruz <lucas.apcruz@gmail.com>
2022-11-02 18:15:37 -0300

    refac: starting with empty tweet database

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ app.use(cors())
 app.use(express.json())
 
 const users = [
-    { username: "bobesponja", avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" }
 ]
+
 const tweets = [
-    { username: "bobesponja", tweet: "eu amo o hub" }
 ]
 
 function isValidUrl(url) {
